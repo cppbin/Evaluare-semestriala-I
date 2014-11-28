c585eac3964cd41da170c7902110f8a1129ae02a
Dana Jebelean <danajebelean@yahoo.com>
2014-11-28 11:58:12 +0200

    Update app_codificare_adauga_litera.cpp

--- app_codificare_adauga_litera.cpp
+++ app_codificare_adauga_litera.cpp
@@ -11,14 +11,14 @@ int main()
     int i, l;
     cout << "cuvantul: "; cin >> s;
     cout << "caracterul: "; cin >> c;
-    p = strchr(s, c);
-    while(p != NULL)
+    p = strchr(s, c); // cauta caracterul c in s
+    while(p != NULL) // cat timp il gaseste
     {
-        strcpy(s1, p+1);
-        s[p-s+1] = c;
-        strcpy(p+2, s1);
-        p = p + 2;
-        p = strchr(p, c);
+        strcpy(s1, p+1); // salveaza in s1 sirul care incepe dupa caracterul gasit
+        s[p-s+1] = c; // completeaza pozitia urmatoare pozitiei pe care a gasit litera c cu caracterul c
+        strcpy(p+2, s1); // pune sirul s1 peste sirul s incepand cu doua pozitii dupa
+        p = p + 2; // pointerul p se muta peste litera c gasita si litera c inserata
+        p = strchr(p, c); // procesul continua de la noul p
     }
     cout << s;
     return 0;
