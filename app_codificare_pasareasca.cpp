8ed8aa12616f45ad00a30066b1c5f6a57ba075b6
Dana Jebelean <danajebelean@yahoo.com>
2014-11-28 11:55:35 +0200

    Update app_codificare_pasareasca.cpp

--- app_codificare_pasareasca.cpp
+++ app_codificare_pasareasca.cpp
@@ -13,15 +13,15 @@ int main()
     l = strlen(s);
     for(i = 0; i < l; i++)
     {
-        p = strchr("aeiou", s[i]);
-        if(p != NULL)
+        p = strchr("aeiou", s[i]); //cauta vocala
+        if(p != NULL) // daca gaseste o vocala
         {
-            strcpy(s1, s+i+1);
-            s[i+1]='p';
-            s[i+2]=p[0];
-            strcpy(s+i+3, s1);
-            i=i+3;
-            l=l+2;
+            strcpy(s1, s+i+1); //salveaza in s1 sirul care incepe dupa vocala
+            s[i+1]='p'; //completeaza in sir pozitia urmatoare gasirii vocalei cu caracterul 'p'
+            s[i+2]=p[0]; //completeaza in sir pe pozitia urmatoare literei 'p' vocala gasita
+            strcpy(s+i+3, s1); //aduce sirul s1 dupa cele doua pozitii completate anterior
+            i=i+3; // i sare peste urmatoarele 3 caractere care nu mai trebuie tratate
+            l=l+2; // lungimea sirului se mareste cu 2, la fiecare gasire de vocala se adauga 2 caractere
         }
     }
     cout << s;
